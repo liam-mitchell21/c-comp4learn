8f8c968df969d48260f07b590d5103b5aaa4c1bf
Liam R Mitchell <liam.mitchell@uleth.ca>
2019-09-19 01:04:45 +0000

    modified compilearn to test command line arguments

--- compilearn.rs
+++ compilearn.rs
@@ -1,1 +1,14 @@
+//this is my attempt at building a c compiler
+//this is written in rust
 
+
+use std:env;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	println!("{:?}", args);
+}
+
+//fn lex() {
+//
+//}
